d39e0531a4d6002c39ce961db9065c3aa43ba6ee
isTorres <ismaeltorres612@gmail.com>
2022-02-16 18:41:26 -0300

    feat: cria componente de listagem de usuarios

---
+++ components/UsersList.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import API from "../services/fetchAPI";
+import CardUser from "../components/CardUser";
+
+export default function UsersList() {
+  const [listUsers, setListUsers] = useState([]);
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const getUsers = async () => {
+    const apiReturn = await API.fetchAPI();
+    setListUsers(apiReturn);
+    return listUsers;
+  };
+
+  const loading = <span>Loading</span>;
+
+  return (
+    <ul>
+      {!listUsers.results
+        ? loading
+        : listUsers.results.map((user, index) => (
+          <li key={ index }>
+            <CardUser
+              userData={user}
+            />;
+          </li>
+        ))
+      }
+    </ul>
+  );
+}
