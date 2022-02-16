6d9c17f1124a0e79b62f598e3dbb65b9bd8bc05d
isTorres <ismaeltorres612@gmail.com>
2022-02-16 17:07:39 -0300

    refactor: remove testes com jest e RTL e volta pro cypress

---
+++ cypress/integration/homPage.spec.js
@@ -0,0 +1,7 @@
+describe('My First Test', () => {
+  it('Visits localhost:3000', () => {
+    cy.visit('http://localhost:3000');
+    cy.contains('Header');
+    cy.contains('Index');
+  })
+})
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 import React from "react";
+import Header from "../components/Header";
 
 export default function index() {
-  return <div>index</div>;
+  return (
+    <div>
+      <Header />
+      <p>Index</p>
+    </div>
+  );
 }
