0d10367fd3fc3c68b65ae8ea088fd5b0eca2ba8b
isTorres <ismaeltorres612@gmail.com>
2022-02-16 02:08:41 -0300

    Fix: lint

--- services/fetchAPI.js
+++ services/fetchAPI.js
@@ -1,15 +1,16 @@
-export const API_URL = 'https://randomuser.me/api/?results=8'
+export const API_URL = "https://randomuser.me/api/?results=8";
 
-const fetchAPI = () => new Promise(function (resolve, reject) {
-  https.get(API_URL, (res) => {
-    res.setEncoding('utf8');
-    let userInfo = '';
-    res.on('data', (data) => {
-      userInfo += data;
+const fetchAPI = () =>
+  new Promise(function (resolve, reject) {
+    https.get(API_URL, (res) => {
+      res.setEncoding("utf8");
+      let userInfo = "";
+      res.on("data", (data) => {
+        userInfo += data;
+      });
+      res.on("error", reject);
+      res.on("close", () => resolve(JSON.parse(userInfo).results[0]));
     });
-    res.on('error', reject);
-    res.on('close', () => resolve(JSON.parse(userInfo).results[0]));
   });
-});
 
-module.exports = { fetchAPI }
+module.exports = { fetchAPI };
