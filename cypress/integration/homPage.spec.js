cae2f04b8bf7edee5d03e6b5ec410ae526396f9d
isTorres <ismaeltorres612@gmail.com>
2022-02-16 21:10:09 -0300

    feat: cria testes para info dos cards de usuario

--- cypress/integration/homPage.spec.js
+++ cypress/integration/homPage.spec.js
@@ -1,10 +1,6 @@
 const URL_HOME = "http://localhost:3000";
 
-describe("Teste da pagina inicial", () => {
-  before(function () {
-    cy.visit(URL_HOME);
-  });
-
+describe("Teste da pagina", () => {
   it("Visita localhost:3000", () => {
     cy.visit(URL_HOME);
   });
@@ -15,6 +11,25 @@ describe("Teste da pagina inicial", () => {
 
   it("verifica componente de listagem de usuarios", () => {
     cy.get("div > ul > li");
-    cy.get('div > ul').children().should('have.length', 8)
+    cy.get("div > ul").children().should("have.length", 8);
+  });
+
+  describe("teste dos cards de usuarios", () => {
+    it("verifica card de usuario retraido", () => {
+      cy.get("#basic-info").children().should("have.length", 3);
+      cy.get("#basic-info").find("h3").not("null");
+    });
+
+    it("verifica #full-info", () => {
+      cy.get("#full-info").find("div").should("have.length", 3);
+    });
+
+    it("verifica #location-info", () => {
+      cy.get("#location-info").contains("Street");
+      cy.get("#location-info").contains("City");
+      cy.get("#location-info").contains("State");
+      cy.get("#location-info").contains("Country");
+      cy.get("#location-info").contains("PostCode");
+    });
   });
 });
